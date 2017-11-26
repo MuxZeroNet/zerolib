0ca236343b3ac0f8c9c9f54dce321fbb8c744874
MuxZeroNet <muxzeronet@users.noreply.github.com>
2017-11-26 07:47:16 +0000

    Add rwlock tests

--- zerolib/run_tests.py
+++ zerolib/run_tests.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import unittest
 import sys
+from pathlib import Path
 
-suite = unittest.TestLoader().discover('./')
+path = str(Path(__file__).parent.absolute())
+suite = unittest.TestLoader().discover(path)
 result = unittest.TextTestRunner(verbosity=2).run(suite)
 
 sys.exit(not result.wasSuccessful())
---
+++ zerolib/tests/test_locks.py
@@ -0,0 +1,49 @@
+import unittest
+from storage import RWLock
+from timeout_decorator import timeout
+from timeout_decorator.timeout_decorator import TimeoutError as ExpectedTimeout
+
+def assert_timeout(seconds):
+    def decorator(func):
+        def f(self, *args, **kwargs):
+            func_timeout = timeout(seconds)(func)
+            with self.assertRaises(ExpectedTimeout):
+                func_timeout(self, *args, **kwargs)
+        return f
+    return decorator
+
+class TestRWLock(unittest.TestCase):
+    @timeout(3)
+    def test_readers(self):
+        rwlock = RWLock()
+        for i in range(10):
+            rwlock.acquire('r')
+        for i in range(10):
+            rwlock.release('r')
+
+        with self.assertRaises(ValueError):
+            rwlock.release('r')
+
+    @assert_timeout(1)
+    def test_context_rw(self):
+        rwlock = RWLock()
+
+        with rwlock.reader():
+            with rwlock.writer():
+                raise AssertionError('Should not enter')
+
+    @assert_timeout(1)
+    def test_context_wr(self):
+        rwlock = RWLock()
+
+        with rwlock.writer():
+            with rwlock.reader():
+                raise AssertionError('Should not enter')
+
+    @assert_timeout(1)
+    def test_context_ww(self):
+        rwlock = RWLock()
+
+        with rwlock.writer():
+            with rwlock.writer():
+                raise AssertionError('Should not enter')
